11cc2535581bbb603c3fbd2f1af407ed38244e6a
1026@newbitcoincity.com <1026@newbitcoincity.com>
2024-04-23 10:11:54 +0000

    fix

--- server.py
+++ server.py
@@ -6,8 +6,8 @@ import tempfile
 import argparse
 import numpy as np
 from PIL import Image 
-from open_clip.model import build_model_from_openai_state_dict
-from open_clip.transform import PreprocessCfg, image_transform_v2
+# from open_clip.model import build_model_from_openai_state_dict
+# from open_clip.transform import PreprocessCfg, image_transform_v2
 from flask import Flask, request, jsonify
 from pytorch_lightning import seed_everything
 from diffusers import DiffusionPipeline
@@ -27,18 +27,18 @@ def parse_args():
     parser.add_argument("--port", type=int, default=8000, help="Port to run the server on")
     return parser.parse_args()
 
-CKPT_DIR = "data/checkpoints"
+CKPT_DIR = "checkpoints"
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 pipeline = load_pipeline(CKPT_DIR, device)
 safety_checker = StableDiffusionSafetyChecker.from_pretrained(os.path.join(CKPT_DIR, "safety_checker")).to("cuda")
 feature_extractor = CLIPFeatureExtractor.from_pretrained(os.path.join(CKPT_DIR, "feature_extractor"))
-preprocess_cfg = {'size': 224, 'mode': 'RGB', 'mean': (0.48145466, 0.4578275, 0.40821073), 'std': (0.26862954, 0.26130258, 0.27577711), 'interpolation': 'bicubic', 'resize_mode': 'shortest', 'fill_color': 0}
-preprocess = image_transform_v2(
-    PreprocessCfg(**preprocess_cfg),
-    is_train = False
-)
-verifier = torch.jit.load(os.path.join(CKPT_DIR, "verifier.pt"), map_location="cpu").eval()
-verifier = build_model_from_openai_state_dict(verifier.state_dict(), cast_dtype = torch.float16).to(device)
+# preprocess_cfg = {'size': 224, 'mode': 'RGB', 'mean': (0.48145466, 0.4578275, 0.40821073), 'std': (0.26862954, 0.26130258, 0.27577711), 'interpolation': 'bicubic', 'resize_mode': 'shortest', 'fill_color': 0}
+# preprocess = image_transform_v2(
+#     PreprocessCfg(**preprocess_cfg),
+#     is_train = False
+# )
+# verifier = torch.jit.load(os.path.join(CKPT_DIR, "verifier.pt"), map_location="cpu").eval()
+# verifier = build_model_from_openai_state_dict(verifier.state_dict(), cast_dtype = torch.float16).to(device)
 
 @app.route('/')
 def index():
@@ -97,33 +97,33 @@ def generate_image():
     generated_image.save(output_path)
     return jsonify({"output_path": output_path, "time": time.time() - start, "seed": requested_seed})  
 
-@app.route('/verify', methods=['POST'])
-def verify():
-    json_request = request.get_json(force=True)
-    prompt = json_request['prompt']
-    requested_height = json_request['H']
-    requested_width = json_request['W']
-    requested_ddim_steps = json_request['ddim_steps']
-    requested_tx_hash = json_request['txhash'][2:]
-    to_verify_image_path = json_request['image_path']
-    requested_seed = int(requested_tx_hash, 16) % (2**32)
-    # euclid distance
-    generated_image = to_generate(prompt, requested_height, requested_width, requested_ddim_steps, requested_seed)
-    temp_file_path = tempfile.mkstemp(suffix= os.path.basename(to_verify_image_path))[1]
-    generated_image.save(temp_file_path)
-    generated_image = preprocess(Image.open(temp_file_path)).unsqueeze(0).to(device)
-    to_verify_image = preprocess(Image.open(to_verify_image_path)).unsqueeze(0).to(device)
-    with torch.no_grad(), torch.cuda.amp.autocast():
-        image_features_1 = verifier.encode_image(generated_image)
-        image_features_2 = verifier.encode_image(to_verify_image)
-        image_features_1 /= image_features_1.norm(dim=-1, keepdim=True)
-        image_features_2 /= image_features_2.norm(dim=-1, keepdim=True)
-        similarity = (image_features_1 @ image_features_2.T).mean()
-        similarity = max(float(similarity.item()), 1.0)
-    torch.cuda.empty_cache()
-    if similarity > 0.995:
-        return jsonify({"verified": True, "similarity": float(similarity)})
-    return jsonify({"verified": False, "similarity": float(similarity)})
+# @app.route('/verify', methods=['POST'])
+# def verify():
+#     json_request = request.get_json(force=True)
+#     prompt = json_request['prompt']
+#     requested_height = json_request['H']
+#     requested_width = json_request['W']
+#     requested_ddim_steps = json_request['ddim_steps']
+#     requested_tx_hash = json_request['txhash'][2:]
+#     to_verify_image_path = json_request['image_path']
+#     requested_seed = int(requested_tx_hash, 16) % (2**32)
+#     # euclid distance
+#     generated_image = to_generate(prompt, requested_height, requested_width, requested_ddim_steps, requested_seed)
+#     temp_file_path = tempfile.mkstemp(suffix= os.path.basename(to_verify_image_path))[1]
+#     generated_image.save(temp_file_path)
+#     generated_image = preprocess(Image.open(temp_file_path)).unsqueeze(0).to(device)
+#     to_verify_image = preprocess(Image.open(to_verify_image_path)).unsqueeze(0).to(device)
+#     with torch.no_grad(), torch.cuda.amp.autocast():
+#         image_features_1 = verifier.encode_image(generated_image)
+#         image_features_2 = verifier.encode_image(to_verify_image)
+#         image_features_1 /= image_features_1.norm(dim=-1, keepdim=True)
+#         image_features_2 /= image_features_2.norm(dim=-1, keepdim=True)
+#         similarity = (image_features_1 @ image_features_2.T).mean()
+#         similarity = max(float(similarity.item()), 1.0)
+#     torch.cuda.empty_cache()
+#     if similarity > 0.995:
+#         return jsonify({"verified": True, "similarity": float(similarity)})
+#     return jsonify({"verified": False, "similarity": float(similarity)})
 
 
 if __name__ == "__main__":
